d258b9b9fc2680f5611050ef12156dfd40301b58
preethipantangi <pantangisaipreethi@gmail.com>
2024-07-10 00:10:11 -0400

    Upadted the card ui

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ pokemons }) => {
     <div className='flex flex-wrap'>
         {
             pokemons.map(pokemon => (
-            <div className='border border-black cursor-pointer hover:shadow-md m-2 p-2 w-52 flex justify-center' key={pokemon.id}>
+            <div className='border shadow-md cursor-pointer hover:shadow-lg m-2 p-2 w-52 flex justify-center' key={pokemon.id}>
                 <div>
                 <img alt={pokemon.name} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}/>
                 <div className='pokemon-name font-bold'>{pokemon.name}</div>
